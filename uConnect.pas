5e1ce6356e29ab71dcb4161bfe8b52f1733f3e56
Victor Carrico <vicarrico@hotmail.com>
2022-04-14 12:18:02 -0300

    Finalizacao conexao

--- uConnect.pas
+++ uConnect.pas
@@ -2,16 +2,18 @@ unit uConnect;
 
 interface
 Uses
-FireDAC.Phys.FB, FireDAC.Comp.Client, System.Classes, FireDAC.DApt, FireDAC.Stan.Def;
+FireDAC.Phys.FB, FireDAC.Comp.Client, System.Classes, FireDAC.DApt, FireDAC.Stan.Def, Datasnap.Provider,
+  Datasnap.DBClient;
 
 type
 TConnection = class(TComponent)
   FBDriver: TFDPhysFBDriverLink;
   Conn: TFDConnection;
   Trans : TFDTransaction;
+  Provider : TDataSetProvider;
   public
   function conectar() : Boolean;
-  function execQuery(pSQL : string) : boolean;
+  function execQuery(pSQL : string; pCds : TClientDataSet) : boolean;
   constructor Create(AOwner: TComponent); override;
   destructor Destroy();
 end;
@@ -23,18 +25,17 @@ uses
 { Conect }
 
 function TConnection.conectar: Boolean;
+var
+path : string;
 begin
-  Conn := TFDConnection.Create(self);
-  FBDriver := TFDPhysFBDriverLink.Create(self);
-  Trans := TFDTransaction.Create(self);
   Conn.Transaction := Trans;
   FBDriver.VendorLib   := '.\fbclient.dll';
   Conn.Params.Add('DriverID=FB');
-  Conn.Params.Add('Server=localhost');
-  Conn.Params.Add('Database=D:\AulasDelphi\Pedidos\DataBase\DB.IB');
-  Conn.Params.Add('User_Name=sysdba');
+  Conn.Params.Add('CharacterSet=win1251');
+  path := GetCurrentDir();
+  Conn.Params.Add('Database=' + path + '\DataBase\DB.FDB');
+  Conn.Params.Add('User_Name=SYSDBA');
   Conn.Params.Add('Password=masterkey');
-  Conn.Open();
   Conn.Connected := True;
 
 end;
@@ -42,28 +43,37 @@ end;
 constructor TConnection.Create(AOwner: TComponent);
 begin
   inherited;
+  Conn     := TFDConnection.Create(self);
+  FBDriver := TFDPhysFBDriverLink.Create(self);
+  Trans    := TFDTransaction.Create(self);
+
 end;
 
 destructor TConnection.Destroy;
 begin
+  if Conn.Connected then
+    Conn.Connected := False;
 
+  FreeAndNil(Conn);
+  FreeAndNil(FBDriver);
+  FreeAndNil(Trans);
 end;
 
-function TConnection.execQuery(pSQL: string): boolean;
+function TConnection.execQuery(pSQL : string; pCds : TClientDataSet) : boolean;
 var
   Query : TFDQuery;
 begin
   try
-    try
-      Query := TFDQuery.Create(nil);
-      Query.Connection := Conn;
-      Query.Transaction := Conn.Transaction;
-      Query.SQL.Text := 'SELECT * FROM ITEM';
-      Query.ExecSQL;
-    except
-      on E : Exception do
-      Showmessage(E.Message);
-    end;
+    Query := TFDQuery.Create(nil);
+
+    Query.Connection := Conn;
+    Query.Transaction := Conn.Transaction;
+    Query.SQL.Text := pSQL;
+
+    Query.Open(pSQL);
+
+    Provider.DataSet := Query;
+    pCds.Data := Provider.Data;
 
   finally
     Freeandnil(Query);
